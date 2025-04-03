96d992ae3509719c43486d23e77d9378af499573
Javier Fabra Garcia <javier.fabra@domatix.com>
2025-04-03 18:22:32 +0200

    .

---
+++ whatsapp_automation/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Whatsapp automation',
+    'category': 'Domatix',
+    'version': '17.0.1.0.0',
+    'author': 'Domatix',
+    'depends': ['whatsapp', 'base_automation'],
+    
+    'data': [
+        'views/ir_actions_server_views.xml',
+        ],
+    'installable': True,
+}
---
+++ whatsapp_automation/models/__init__.py
@@ -0,0 +1,2 @@
+from . import ir_actions_server
+from . import whatsapp_account
